9f77899b46b34e35e6584eaccf4e551b710f4536
Edward Park <edwardpark123@gmail.com>
2021-03-29 19:20:29 -0700

    merged anthonys auth branch to my ReactRouter branch

---
+++ client/SignIn.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+  Link as RouteLink,
+  withRouter,
+  useHistory,
+  RouteComponentProps
+} from 'react-router-dom';
+
+const SignIn = () => {
+
+  return (
+    <div>
+      Sign In page     
+    </div>
+  );
+};
+
+export default withRouter(SignIn);
